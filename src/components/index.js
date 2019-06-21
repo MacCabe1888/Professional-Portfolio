b36c8b5a27d5a908f04a3876a0431e065f063116
Cameron <cameronjmaccabe@gmail.com>
2019-06-20 21:29:01 -0700

    added new components to list of exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,19 @@
-import Dossier from "./Dossier";
-import NavLeft from "./NavLeft";
-import NavRight from "./NavRight";
-import PageLeft from "./PageLeft";
-import PageRight from "./PageRight";
+import About from "./About/index";
+import Contact from "./Contact/index";
+import Dossier from "./Dossier/index";
+import Nav from "./Nav/index";
+import NoMatch from "./NoMatch/index";
+import Page from "./Page/index";
+import Portfolio from "./Portfolio/index";
+import Skills from "./Skills/index";
 
 export {
+  About,
+  Contact,
   Dossier,
-  NavLeft,
-  NavRight,
-  PageLeft,
-  PageRight
+  Nav,
+  NoMatch,
+  Page,
+  Portfolio,
+  Skills
 };
