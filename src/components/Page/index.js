567f69f2db42708bb2f23ea2fc583a09a5ceae66
Cameron <cameronjmaccabe@gmail.com>
2019-06-20 21:25:46 -0700

    combined PageLeft and PageRight into one Page component using this.props.page to distinguish functionally between the left-hand and right-hand pages, gave the Page component its own folder with a CSS file

---
+++ src/components/Page/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.page} page of {this.props.pageName}
+      </div>
+    );
+  }
+}
+
+export default Page;
