ef7d76243d36eca865ccf21b3d54aa661dc6af43
Cameron <cameronjmaccabe@gmail.com>
2019-06-24 23:09:44 -0700

    added {this.props.children} to Page component to allow insertion of modular, page-specific HTML content from outside Page/index.js

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -6,6 +6,7 @@ class Page extends Component {
     return (
       <div className="page">
         {this.props.page} page of {this.props.pageName}
+        {this.props.children}        
       </div>
     );
   }
