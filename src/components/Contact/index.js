e945a48e661da5a0967003aab19d9c8cac7b2b98
Cameron <cameronjmaccabe@gmail.com>
2019-06-26 22:49:53 -0700

    added contact info

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,7 +12,17 @@ class Contact extends Component {
         />
         <div className="pages">
           <Page pageName="contact" page="left" />
-          <Page pageName="contact" page="right" />
+          <Page pageName="contact" page="right">
+            <h2>
+              Get in Touch
+            </h2>
+            <p>
+              EMAIL: cameronjmaccabe@gmail.com
+            </p>
+            <p>
+              PHONE: 925 899 5574
+            </p>
+          </Page>
         </div>
         <Nav
           class="nav-right"
