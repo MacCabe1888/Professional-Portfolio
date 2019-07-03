15348096695e4b779680bf89417b00fac1ae5568
Cameron <cameronjmaccabe@gmail.com>
2019-07-03 14:29:52 -0700

    added three projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,7 +3,10 @@ import { Nav, Page, Project } from "../";
 import "./style.css";
 
 import burgers from "../../images/Burgers-to-Eat.png";
+import friends from "../../images/Friend-Finder.jpg";
 import bamazon from "../../images/Bamazon.png";
+import wordGuess from "../../images/Constructor-Word-Guess.png";
+import crystals from "../../images/Crystal-Collector-Game.jpg";
 
 class Projects extends Component {
   render() {
@@ -23,10 +26,28 @@ class Projects extends Component {
               name="Burgers to Eat"
             />
             <Project
+              siteURL="https://friendfinder1888.herokuapp.com/"
+              repoURL="https://github.com/MacCabe1888/Friend-Finder"
+              imgSrc={friends}
+              name="Friend Finder"
+            />
+            <Project
               siteURL="https://github.com/MacCabe1888/Bamazon"
               repoURL="https://github.com/MacCabe1888/Bamazon"
               imgSrc={bamazon}
               name="Bamazon"
+            />
+            <Project
+              siteURL="https://github.com/MacCabe1888/Constructor-Word-Guess"
+              repoURL="https://github.com/MacCabe1888/Constructor-Word-Guess"
+              imgSrc={wordGuess}
+              name="Constructor Word Guess"
+            />
+            <Project
+              siteURL="https://maccabe1888.github.io/Crystal-Collector-Game/"
+              repoURL="https://github.com/MacCabe1888/Crystal-Collector-Game"
+              imgSrc={crystals}
+              name="Crystal Collector Game"
             />
           </Page>
         </div>
