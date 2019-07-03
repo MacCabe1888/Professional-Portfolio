1f2320171348634ce0be98570b728dabf1a78a68
Cameron <cameronjmaccabe@gmail.com>
2019-07-03 14:28:32 -0700

    added target="_blank" and rel="noopener noreferrer" to links in order to open project-related pages on new tabs

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,13 +5,23 @@ class Project extends Component {
   render() {
     return (
       <div className="project">
-        <a href={this.props.siteURL}>
-          <img
-            src={this.props.imgSrc}
-            alt={this.props.name}
-          />
+        <a
+          href={this.props.siteURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="img-container">
+            <img
+              src={this.props.imgSrc}
+              alt={this.props.name}
+            />
+          </div>
         </a>
-        <a href={this.props.repoURL}>
+        <a
+          href={this.props.repoURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <p>
             {this.props.name}
           </p>
