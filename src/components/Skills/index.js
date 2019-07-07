88895f655aeef164d7144984c0d422d384a838c5
Cameron <cameronjmaccabe@gmail.com>
2019-07-06 23:55:22 -0700

    added two skills to Skills page

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { Nav, Page } from "../";
 import "./style.css";
 
+import logo from "../../logo.svg";
+import js from "../../js.svg";
+
 class Skills extends Component {
   render() {
     return (
@@ -11,8 +14,29 @@ class Skills extends Component {
           pageIndex={1}
         />
         <div className="pages">
-          <Page pageName="skills" page="left" />
-          <Page pageName="skills" page="right" />
+          <Page />
+          <Page>
+            <h2>
+              Skills
+            </h2>
+            <section id="skill-1" className="skill">
+              <img className="skill" src={logo} alt="React Logo" />
+              React
+            </section>
+            <section id="skill-2" className="skill">
+              <img className="skill" src={js} alt="JavaScript Logo" />
+              JavaScript
+            </section>
+            <section id="skill-3" className="skill">
+
+            </section>
+            <section id="skill-4" className="skill">
+
+            </section>
+            <section id="skill-5" className="skill">
+
+            </section>
+          </Page>
         </div>
         <Nav
           className="nav-right"
