1bb6b3508d997f1395099d4f2da9c36b1899595e
Cameron <cameronjmaccabe@gmail.com>
2019-07-09 12:52:33 -0700

    added classes to each page for more specific CSS targeting

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Page extends Component {
+  pageClasses = `page ${this.props.type}`
+
   render() {
     return (
-      <div className="page">
+      <div className={this.pageClasses} id={this.props.id}>
         {this.props.children}        
       </div>
     );
