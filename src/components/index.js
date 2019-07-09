9b17aa149ebf54bbdb737959130ae74a1dfc79db
Cameron <cameronjmaccabe@gmail.com>
2019-07-09 12:58:29 -0700

    added left and right page componentsto list of exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,37 @@
 import About from "./About/index";
+import AboutLeft from "./About/left";
+import AboutRight from "./About/right";
 import Contact from "./Contact/index";
+import ContactLeft from "./Contact/left";
+import ContactRight from "./Contact/right";
 import Dossier from "./Dossier/index";
 import Nav from "./Nav/index";
 import NoMatch from "./NoMatch/index";
 import Page from "./Page/index";
 import Project from "./Project/index";
 import Projects from "./Projects/index";
+import ProjectsLeft from "./Projects/left";
+import ProjectsRight from "./Projects/right";
 import Skills from "./Skills/index";
+import SkillsLeft from "./Skills/left";
+import SkillsRight from "./Skills/right";
 
 export {
   About,
+  AboutLeft,
+  AboutRight,
   Contact,
+  ContactLeft,
+  ContactRight,
   Dossier,
   Nav,
   NoMatch,
   Page,
   Project,
   Projects,
-  Skills
+  ProjectsLeft,
+  ProjectsRight,
+  Skills,
+  SkillsLeft,
+  SkillsRight
 };
