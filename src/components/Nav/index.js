be2816380ded2e0718d7f0ca8bec381cac8eeef4
Cameron <cameronjmaccabe@gmail.com>
2019-07-09 12:49:22 -0700

    added handleOnClick function to generate appropriate page turn animation based on current page and nav tab selected

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./style.css";
 
 class Nav extends Component {
@@ -7,6 +7,7 @@ class Nav extends Component {
     super(props);
     this.pages = ["about", "skills", "projects", "contact"];
     this.pageName = window.location.href.split("/").pop();
+    this.state = { animating: false };
   }
 
   hasTab = this.props.className === "nav-left"
@@ -17,17 +18,59 @@ class Nav extends Component {
       return pageIndex >= this.props.pageIndex;
     };
 
+  handleOnClick = (event, pageName) => {
+    event.preventDefault();
+    if (pageName !== this.pageName && !this.state.animating) {
+      const activateTabs = this.props.className === "nav-left"
+      ? () => {
+          document.getElementsByClassName("left-side")[0].classList.add("active");
+          document.getElementById(`${pageName}-right`).classList.replace("hidden", "flipped");
+          document.getElementById(`${pageName}-left`).classList.replace("hidden", "unveiled");
+          const tabs = document.getElementsByClassName("nav-tab left");
+          for (let i = 0; i < tabs.length; i++) {
+            if (this.pages.indexOf(tabs[i].textContent.toLowerCase()) >= this.pages.indexOf(pageName)) {
+              tabs[i].classList.add("active");
+            };
+          }
+        }
+      : () => {
+          document.getElementsByClassName("right-side")[0].classList.add("active");
+          document.getElementById(`${pageName}-left`).classList.replace("hidden", "flipped");
+          document.getElementById(`${pageName}-right`).classList.replace("hidden", "unveiled");
+          const tabs = document.getElementsByClassName("nav-tab right");
+          for (let i = 0; i < tabs.length; i++) {
+            if (this.pages.indexOf(tabs[i].textContent.toLowerCase()) < this.pages.indexOf(pageName)) {
+              tabs[i].classList.add("active");
+            };
+          }
+        };
+      activateTabs();
+      this.setState({ animating: true });
+      window.setTimeout(() => {
+        this.props.history.push(pageName);
+      }, 1000);
+    }
+  };
+
   render() {
     if (this.pageName && !this.pages.includes(this.pageName)) {
-      return (<div />);
+      return;
     }
     return (
       <nav>
         {this.pages.map((pageName, pageIndex) => {
           return (
             this.hasTab(pageIndex)
-            ? <Link key={pageIndex} to={"/" + pageName}>
-                <button className="nav-tab">
+            ? <Link
+                key={pageIndex}
+                to={"/" + pageName}
+                onClick={event => this.handleOnClick(event, pageName)}
+              >
+                <button
+                  className={`nav-tab ${this.props.className === "nav-left"
+                  ? "left"
+                  : "right"}`}
+                >
                   <p className="nav-text">
                     {pageName.toUpperCase()}
                   </p>
@@ -44,4 +87,4 @@ class Nav extends Component {
   }
 }
 
-export default Nav;
+export default withRouter(Nav);
