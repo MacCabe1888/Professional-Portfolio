63b6e9b8335ed3018a7080aec8f5d76b8230f9b6
Cameron <cameronjmaccabe@gmail.com>
2019-07-13 22:43:05 -0700

    added three skills

--- src/components/Skills/right.js
+++ src/components/Skills/right.js
@@ -3,7 +3,10 @@ import { Page } from "../";
 import "./style.css";
 
 import logo from "../../logo.svg";
+import node from "../../node.svg"
 import js from "../../js.svg";
+import mongo from "../../mongodb.svg";
+import sql from "../../sql.svg";
 
 class SkillsRight extends Component {
   render() {
@@ -14,20 +17,23 @@ class SkillsRight extends Component {
         </h2>
         <section id="skill-1" className="skill">
           <img className="skill" src={logo} alt="React Logo" />
-          React
+          <p>React</p>
         </section>
         <section id="skill-2" className="skill">
-          <img className="skill" src={js} alt="JavaScript Logo" />
-          JavaScript
+          <img className="skill" src={node} alt="Node.js Logo" />
+          <p>Node.js</p>
         </section>
         <section id="skill-3" className="skill">
-
+          <img className="skill" src={js} alt="JavaScript Logo" />
+          <p>JavaScript</p>
         </section>
         <section id="skill-4" className="skill">
-
+          <img className="skill" src={mongo} alt="MongoDB Logo" />
+          <p>MongoDB</p>
         </section>
         <section id="skill-5" className="skill">
-
+          <img className="skill" src={sql} alt="SQL Logo" />
+          <p>SQL</p>
         </section>
       </Page>
     );
