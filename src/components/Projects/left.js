e7302484be15d0751591b0472e18e14ff4d4f514
Cameron <cameronjmaccabe@gmail.com>
2019-07-17 15:26:48 -0700

    added project template as a guide for the user

--- src/components/Projects/left.js
+++ src/components/Projects/left.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
-import { Page } from "../";
+import { Page, Project } from "../";
 import "./style.css";
+
+import sitePlaceholder from "../../assets/images/sitePlaceholder.png";
+
+import techPlaceholder from "../../assets/images/techPlaceholder.png";
 
 class ProjectsLeft extends Component {
   render() {
     return (
       <Page type={this.props.type} id="projects-left">
-        [LEFT PAGE OF PROJECTS]
+        <h2>
+          Project Template
+        </h2>
+        <Project
+          imgSrc={sitePlaceholder}
+          name="Project Name (Click to view the project repository on GitHub.)"
+          description="Project Description"
+          tech={[{
+            src: techPlaceholder,
+            alt: "Tech Logo"
+          }]}
+        />
       </Page>
     );
   }
