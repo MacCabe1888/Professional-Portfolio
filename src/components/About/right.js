edbe282c6221ab0f84f1693f4092366216001519
Cameron <cameronjmaccabe@gmail.com>
2019-07-18 23:34:33 -0700

    separated bio and location info into distinct sections

--- src/components/About/right.js
+++ src/components/About/right.js
@@ -9,18 +9,22 @@ class AboutRight extends Component {
         <h2>
           About Me
         </h2>
-        <p>
-          I graduated <i>magna cum laude</i> from Columbia University with a Bachelor of Arts degree in Philosophy and Music.
-        </p>
-        <p>
-          In May 2019, I completed my Web Development certification through UC Berkeley Extension's full-time, full-stack, full-night's-sleep-depriving coding boot camp.
-        </p>
-        <p>
-          While full-stack web development is currently my main focus, I am also curious about AI programming and video game development.
-        </p>
-        <p>
-          LOCATION: San Francisco Bay Area, CA
-        </p>
+        <section id="bio">
+          <p>
+            I graduated <i>magna cum laude</i> from Columbia University with a Bachelor of Arts degree in Philosophy and Music.
+          </p>
+          <p>
+            In May 2019, I completed my Web Development certification through UC Berkeley Extension's full-time, full-stack, full-night's-sleep-depriving coding boot camp.
+          </p>
+          <p>
+            While full-stack web development is currently my main focus, I am also curious about AI programming and video game development.
+          </p>
+        </section>
+        <section id="location">
+          <p>
+            LOCATION: San Francisco Bay Area, CA
+          </p>
+        </section>
       </Page>
     );
   }
