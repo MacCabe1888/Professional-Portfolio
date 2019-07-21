bdc2c7660b7d561142b793a7a0b67e37402758f6
Cameron <cameronjmaccabe@gmail.com>
2019-07-20 23:45:24 -0700

    added additional skills

--- src/components/Skills/right.js
+++ src/components/Skills/right.js
@@ -2,38 +2,43 @@ import React, { Component } from "react";
 import { Page } from "../";
 import "./style.css";
 
-import logo from "../../logo.svg";
 import node from "../../node.svg";
-import js from "../../js.svg";
+import express from "../../express.svg";
 import mongo from "../../mongodb.svg";
-import sql from "../../sql.svg";
+import mysql from "../../mysql.svg";
+import sequelize from "../../sequelize.svg";
+import handlebars from "../../handlebars.svg";
 
 class SkillsRight extends Component {
   render() {
     return (
       <Page type={this.props.type} id="skills-right">
         <h2>
-          Skills
+          Additional Skills
         </h2>
-        <section id="skill-1" className="skill">
-          <img className="skill" src={logo} alt="React Logo" />
-          <p>React</p>
-        </section>
-        <section id="skill-2" className="skill">
+        <section className="skill">
           <img className="skill" src={node} alt="Node.js Logo" />
           <p>Node.js</p>
         </section>
-        <section id="skill-3" className="skill">
-          <img className="skill" src={js} alt="JavaScript Logo" />
-          <p>JavaScript</p>
+        <section className="skill">
+          <img className="skill" src={express} alt="Express Logo" />
+          <p>Express</p>
         </section>
-        <section id="skill-4" className="skill">
+        <section className="skill">
           <img className="skill" src={mongo} alt="MongoDB Logo" />
           <p>MongoDB</p>
         </section>
-        <section id="skill-5" className="skill">
-          <img className="skill" src={sql} alt="SQL Logo" />
-          <p>SQL</p>
+        <section className="skill">
+          <img className="skill" src={mysql} alt="MySQL Logo" />
+          <p>MySQL</p>
+        </section>
+        <section className="skill">
+          <img className="skill" src={sequelize} alt="Sequelize Logo" />
+          <p>Sequelize</p>
+        </section>
+        <section className="skill">
+          <img className="skill" src={handlebars} alt="Handlebars Logo" />
+          <p>Handlebars</p>
         </section>
       </Page>
     );
