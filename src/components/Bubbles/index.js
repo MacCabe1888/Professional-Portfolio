d70d406ea32ca22c3ded91c78a03d0b72e117036
Cameron <cameronjmaccabe@gmail.com>
2019-08-14 14:36:30 -0700

    added HTML for bubble animation

---
+++ src/components/Bubbles/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Bubbles extends Component {
+  render() {
+    return (
+      <ul className="bubbles">
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+      </ul>
+    )
+  }
+}
+
+export default Bubbles;
